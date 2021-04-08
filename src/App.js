a1df5780ee0326b3b64f57d8fc0bc1d88a30f316
mattbmoorin <tzaratape@gmail.com>
2021-04-07 23:01:58 -0500

    ye

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Controller from './components/Controller';
 import Header from './components/Header';
+import Oscillator from './components/Oscillator';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Oscillator />
       <Controller test="Testing!" />
     </div>
   );
--- src/components/Oscillator.js
+++ src/components/Oscillator.js
@@ -1,5 +1,19 @@
+import * as Tone from 'tone';
+
+const synth = new Tone.Synth().toMaster();
+
 const Oscillator = () => {
-  return <div></div>;
+  return (
+    <div id="wrapper">
+      <button id="button" onClick={playSynth}>
+        Note
+      </button>
+    </div>
+  );
+};
+
+const playSynth = () => {
+  synth.triggerAttackRelease('C2', 0.05);
 };
 
 export default Oscillator;
